d2d5cb8d3b99a11fd0d2274ebd39fbb150d239c2
Sanjit Singh <ss3448@scarletmail.rutgers.edu>
2022-04-02 23:17:34 -0400

    integrated ardity & unity robotics simulation library. gonna work on telerobotics control

---
+++ UnityArdity/Unity-Hardware/Assets/Scripts/My1stScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class My1stScript : MonoBehaviour {
+    private float AmountToMove;
+    SerialPort serial = new SerialPort("COM3", 9600);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!serial.IsOpen)
+            serial.Open();
+
+        int data = int.Parse(serial.ReadLine());
+        AmountToMove = 2;
+        MoveObject(data);
+    }
+
+    void MoveObject(int direction)
+    {
+        if(direction == 1) {
+            transform.Translate(Vector3.left * AmountToMove, Space.World);
+
+        }
+        if (direction == 2) {
+            transform.Translate(Vector3.right * AmountToMove, Space.World);
+
+        }
+
+
+    }
+}
